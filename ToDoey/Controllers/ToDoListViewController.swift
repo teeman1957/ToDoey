b5fb1ea820ed1d877a086a55f3fd3f36f505221b
Anthony Shaw <teeman1957@charter.net>
2018-04-19 13:59:49 -0500

    Converted to MVC design pattern.

--- ToDoey/Controllers/ToDoListViewController.swift
+++ ToDoey/Controllers/ToDoListViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Buy Mild", "Buy Eggs", "Buy Bread", "A long text string that I hope is too big for the display so we can see what happens"]
+    var itemArray = [Item]()
+    // var itemArray = ["Item1", "Item2", "Item3"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
+//        let newItem = Item()
+//        newItem.title = "Find Mike"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Buy Eggs"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.title = "Destroy Demogorgon"
+//        itemArray.append(newItem3)
+        
+//            print(itemArray)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
+    
     }
 
     // Tableview Datasource Methods
@@ -27,10 +48,22 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        // setup constant to use instead of repeating itemArray[indexPath.row] in the subsequent statements
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        // use ternary operator to cut down the below code
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+//        return cell
+// is replaced with below ternary operator line if cell.accessoryType = item.done is true then set to 1st value else 2nd value
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
-        
     }
     
     // Tableview delegate methods
@@ -38,12 +71,26 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        /* below is the long method for setting the done Boolean
+        if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        } else {
+            itemArray[indexPath.row].done = false
+        }
+         
+         the next method is shorter and cleaner
+        */
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+        tableView.reloadData()
+        
+        /* if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
+        */
         // stop it from staying grey, instead flashes grey then retruns to white
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -58,7 +105,11 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once user clicks the Add Item button
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Anthony Shaw on 4/16/18.
+//  Copyright © 2018 Anthony Shaw. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
